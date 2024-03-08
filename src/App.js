574f9760acebdc0116e444b5626b9d8bad45919c
isabellebryans <bryansi@tcd.ie>
2024-03-08 17:58:43 +0000

    Include example files to download

--- src/App.js
+++ src/App.js
@@ -8,6 +8,14 @@ function App() {
       <header className="App-header">
         <h1>Ethical LOD Checker</h1>
         <DragDropFileUpload />
+        <div className="example-files">
+          <h2>Example Files</h2>
+          <p>Download and drag these example files into the upload box:</p>
+          <ul>
+            <li><a href={`${process.env.PUBLIC_URL}/streetCrimeCamden.rdf`} download="streetCrimeCamden.rdf">Example File 1</a></li>
+            {/* Add more files as needed */}
+          </ul>
+        </div>
       </header>
     </div>
   );
