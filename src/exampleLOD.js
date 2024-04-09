a6a535d5f160725db2d0bd8fb9e6a367fa4ffaa0
Isabelle Bryans <72135229+isabellebryans@users.noreply.github.com>
2024-04-09 14:43:03 +0100

    Update exampleLOD.js

--- src/exampleLOD.js
+++ src/exampleLOD.js
@@ -15,7 +15,7 @@ const exampleFileGroups = [
         { name: "Unemployment Rate", path: "/czso-unemployment-rate.rdf" },
         { name: "Unemployment Rate Ontology", path: "/czso-unemployment-rate-ontology.rdf" },
         { name: "Average Salaries", path: "/czso-average-salaries.rdf" },
-        { name: "Average Salaries Ontology", path: "/czso-avg-salaries-ontology.rdf" }
+        { name: "Average Salaries Ontology", path: "/czso-avg-salaries-ontology.rdf" },
         { name: "Deaths by Cause of Death", path: "/czso-deaths-by-cause.rdf" },
         { name: "Deaths by Cause of Death Ontology", path: "/czso-deaths-by-cause-ontology.rdf" }
         // Add more files as needed
